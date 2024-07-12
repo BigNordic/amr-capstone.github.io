ce4f7ee8f758c8aad60a6048935f900db0f4b180
BigNordic <126308247+BigNordic@users.noreply.github.com>
2024-07-12 09:30:34 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+    fetch('https://newsapi.org/v2/everything?q=antimicrobial resistance&apiKey=b0495b83a9274dfda5eaf729f7593a13').then((res) => {
+        return res.json();
+    }).then((data) => {
+
+        let images = ['2-pseudomonas-aeruginosa-bacteria-cdcscience-photo-library.jpg', '1418859086_30426f75d2_z.jpgzz1_.jpeg', 'amr-1.jpg', 'amr-2.webp', 'amr-3.webp', 'iStock-1173815937.jpg', 'pseudomonas-aeruginosa-in-petri-dish.jpg', 'TEM-images-of-Pseudomonas-aeruginosa-with-30000x-magnification-a-without-any.png']
+        let news = document.getElementById('news');
+        let content = [];
+        for(let i = 0; i < images.length; i++){
+            let container = document.createElement('div');
+            container.classList.add('imgContainer');
+        
+            let img = document.createElement('img');
+            img.setAttribute('src', images[i]);
+            img.classList.add('newsImg');
+        
+            let link = document.createElement('a');
+            link.classList.add('newsLink');
+            link.setAttribute('href', `${data.articles[i].url}`);
+            link.innerHTML = data.articles[i].title;
+            
+            container.appendChild(img);
+            container.appendChild(link);
+        
+            content.push(container)
+        
+        if((i % 2 == 1) || ((i == images.length - 1) && (images.length % 2 == 1))){
+            div = document.createElement('div');
+            div.classList.add('img-row');
+            for(let i = 0; i < content.length; i++){
+                div.appendChild(content[i]);
+            }
+            content = [];
+            news.appendChild(div);
+        }
+        }
+})
