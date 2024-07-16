b846d631ad73f1e804c1093f72ccf3145c8094a4
BigNordic <126308247+BigNordic@users.noreply.github.com>
2024-07-16 09:43:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyOb2ck5s25zAgJ9uA7ngFGOkjC2MZcdbeQ7A9PefTgZ8VQ0MbGSwUcD8ssGGmhv6k1/exec'
 
-const form = document.forms['contact-form']
+const form = document.forms['contact-form'];
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+  e.preventDefault();
+  let inputted = 0;
+
+  let qs = document.querySelectorAll('.q');
+  console.log(qs)
+
+  for(let i = 0; i < qs.length; i++){
+    if(qs[i].classList.contains('radio')){
+      for(let j = 0; j < qs[i].children.length; j++){
+        if(qs[i].children[j].checked){
+          console.log(qs[i].children[j])
+          inputted += 1;
+          j = qs[i].children.length;
+        }
+      }
+    } else {
+      if(qs[i].value != ''){
+        inputted += 1;
+        console.log(qs[i])
+      }
+    }
+  }
+
+  if(inputted == qs.length){
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thank you! your form is submitted successfully." ))
+    .then(() => { })
+    .catch(error => console.error('Error!', error.message))
+  } else {
+    alert('answer all questions before you submit')
+  }
 })
